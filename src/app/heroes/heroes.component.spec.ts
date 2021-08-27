fc80c5204e8d67131f8d49d4027dfc5572554ed7
andreyv99 <andreyworonov@yahoo.com>
2021-08-27 11:58:12 +0300

    commit

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,40 @@
+import { of } from "rxjs";
+import { HeroesComponent } from "./heroes.component"
+
+describe('HeroesComponent', () => {
+    let component: HeroesComponent;
+    let HEROES;
+    let mockHeroService;
+
+    beforeEach(() => {
+        HEROES = [
+            {id:1, name: 'SpiderDude', strength: 8},
+            {id:2, name: 'Batman', strength: 24},
+            {id:3, name: 'SuperDude', strength: 55}
+        ]
+
+        mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero'])
+        component = new HeroesComponent(mockHeroService);
+    })
+
+    describe('delete', () => {
+        it('should remove the indicated hero from the heroes list', () => {
+            mockHeroService.deleteHero.and.returnValue(of(true))
+            component.heroes = HEROES;
+
+            component.delete(HEROES[2]);
+
+            expect(component.heroes.includes(HEROES[2])).toBeFalsy();
+        })
+
+        it('should call deleteHero', () => {
+            mockHeroService.deleteHero.and.returnValue(of(true))
+            component.heroes = HEROES;
+
+            component.delete(HEROES[2]);
+
+            expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
+        })
+
+    })
+})
